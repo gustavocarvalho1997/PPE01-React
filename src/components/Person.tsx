12d7191ff39e4450ee037b17989b55e20c2b83af
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-02-08 18:35:29 -0300

    aula_04

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,15 +1,26 @@
+const getWeekday = (today: Date): string => {
+    return new Intl.DateTimeFormat('pt-BR', { weekday: 'long' }).format(today);
+}
+
 export const Person = () => {
+    const data = {
+        name: "Calo",
+        legs: 2,
+        avatar: "https://i.pinimg.com/736x/4d/5c/34/4d5c345462e46789b9d170487d85ccca.jpg"
+    }
+    const today: Date = new Date();
+
     return (
         <>
-            <h1>Calo</h1>
+            <h1>{data.name} - {getWeekday(today)}</h1>
             <img 
-                src="https://i.pinimg.com/736x/4d/5c/34/4d5c345462e46789b9d170487d85ccca.jpg" 
+                src={data.avatar} 
                 alt="Imagem do calo" 
                 className="w-52"    
             />
             <ul>
-                <li>Nome: Calo</li>
-                <li>Qtd Pernas: 2</li>
+                <li>Nome: {data.name}</li>
+                <li>Qtd Pernas: {data.legs}</li>
             </ul>
         </>
     )
