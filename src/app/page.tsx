61543fbb9bd122d6a9675111bef25390b807417d
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-02-08 18:14:55 -0300

    aula 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { GeoForm } from '@/components/GeoForm'
+import { Person } from '@/components/Person'
 
 export default function Page() {
   return (
     <div>
       <GeoForm />
+      <Person />
     </div>
   )
 }
---
+++ src/components/Person.tsx
@@ -0,0 +1,16 @@
+export const Person = () => {
+    return (
+        <>
+            <h1>Calo</h1>
+            <img 
+                src="https://i.pinimg.com/736x/4d/5c/34/4d5c345462e46789b9d170487d85ccca.jpg" 
+                alt="Imagem do calo" 
+                className="w-52"    
+            />
+            <ul>
+                <li>Nome: Calo</li>
+                <li>Qtd Pernas: 2</li>
+            </ul>
+        </>
+    )
+}
