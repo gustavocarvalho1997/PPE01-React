4fd6d52b1344e15aa7082c254d7e16c5af40f566
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-02-09 00:36:57 -0300

    aula_10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,20 @@
 import React from 'react'
-import { GeoForm } from '@/components/GeoForm'
-import { Person } from '@/components/Person'
-import { Card } from '@/components/Card'
+import { peopleList } from '@/data/peopleList';
+
 
 export default function Page() {
   return (
     <div>
-      <GeoForm />
-      <Person 
-        name="Calo" 
-        legs={2} 
-        avatar="https://i.pinimg.com/736x/4d/5c/34/4d5c345462e46789b9d170487d85ccca.jpg"
-      />
-      <Person 
-        name="Jaré" 
-        legs={2} 
-        avatar="https://pbs.twimg.com/media/EGPHaO9WoAA4vAR.jpg"
-      />
-      <Person 
-        name="Cosmo" 
-        legs={2} 
-      />
-      <Card 
-        phrase='Não é o mais forte das espécies que sobrevive, nem o mais inteligente, mas o que melhor se adapta às mudanças.'
-        author='Charles Darwin'
-      />
+      <h1 className="text-4xl font-bold text-center">People List</h1>
+      {peopleList.length > 0 &&
+        <ul>
+        {peopleList.map(person => (
+          <li key={person.id} className="text-xl">
+            {person.name} - {person.professsion}
+          </li>
+        ))}
+        </ul>
+      }
     </div>
   )
 }
---
+++ src/data/peopleList.ts
@@ -0,0 +1,9 @@
+import { Person } from "@/types/Person";
+
+export const peopleList: Person[] = [
+    { id: 1, name: 'Augusto', professsion: 'Pedreiro'},
+    { id: 2, name: 'João', professsion: 'Engenheiro'},
+    { id: 3, name: 'Maria', professsion: 'Médica'},
+    { id: 4, name: 'Pedro', professsion: 'Advogado'},
+    { id: 5, name: 'Luiza', professsion: 'Professora'}
+];
